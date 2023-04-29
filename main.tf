bc9fd96645dd676bf7d2799ba9352dddaca4f08f
Amir Marmul <amiruddinmarmul@gmail.com>
2023-04-29 13:50:27 +0800

    add key_pair

--- main.tf
+++ main.tf
@@ -13,9 +13,15 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = var.key_pair_name
+  public_key = var.ssh_public_key
+}
+
 resource "aws_instance" "app_server" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name      = aws_key_pair.my_key_pair.key_name
 
   tags = {
     Name = var.instance_name
--- variables.tf
+++ variables.tf
@@ -1,23 +1,29 @@
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-southeast-1"
+  type    = string
+  default = "ap-southeast-1"
 }
 
 variable "ami" {
-  description = "ID of the AMI to provision. Default is Ubuntu Server 20.04 LTS (HVM)"
-  type        = string
-  default     = "ami-062550af7b9fa7d05"
+  type    = string
+  default = "ami-062550af7b9fa7d05"
 }
 
 variable "instance_type" {
-  description = "Value of the type of the EC2 instance"
-  type        = string
-  default     = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "Provisioned by Terraform"
+  type    = string
+  default = "Provisioned by Terraform"
+}
+
+variable "key_pair_name" {
+  type    = string
+  default = "amir-pc"
+}
+
+variable "ssh_public_key" {
+  type    = string
+  default = ""
 }
